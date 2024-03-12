2570bd439b9e263c8d75e67c773210267e59b904
2CT <maksszkorupa@Interia.pl>
2024-03-12 09:45:05 +0100

    koniec petli

--- scripts.js
+++ scripts.js
@@ -3,33 +3,33 @@ function ubrania(){
     let ubrania = [
         {
             url: "https://admor.co/media/catalog/product/cache/7af54c596c9583b1b3d45b2de6b91dc3/h/o/horse-hoodie-black.webp",
-            cena: "sdsd",
-            opis: "sdsd"
+            cena: "399 zł",
+            opis: "HORSE HEAVY HOODIE (BLACK)"
         },
         {
             url: "https://admor.co/media/catalog/product/cache/7af54c596c9583b1b3d45b2de6b91dc3/0/0/001_2_.webp",
-            cena: "",
-            opis: ""
+            cena: "379 zł",
+            opis: "INK CRYSTALS HOODIE (BROWN)"
         },
         {
             url: "https://admor.co/media/catalog/product/cache/7af54c596c9583b1b3d45b2de6b91dc3/p/l/plummet.webp",
-            cena: "",
-            opis: ""
+            cena: "349 zł",
+            opis: "PLUMMET HOODIE (GREY)"
         },
         {
-            url: "",
-            cena: "",
-            opis: ""
+            url: "https://www.datocms-assets.com/24293/1659422552-off-white-nike-air-force-1-low-grey-release-date-mock-kopie.png?fm=jpg&max-h=420&max-w=645",
+            cena: "660 zł",
+            opis: "AIR FORCE 1 GRAY "
         },
         {
-            url: "",
-            cena: "",
-            opis: ""
+            url: "https://www.nicekicks.com/files/2023/12/air-jordan-4-white-thunder-fq8138-001-1.jpeg",
+            cena: "1379 zł",
+            opis: "AIR JORDAN 4 WHITE THUNDER"
         },
         {
-            url: "",
-            cena: "",
-            opis: ""
+            url: "https://sneakersdepot.pl/cdn/shop/products/Air-Jordan-5-Gore-Tex-Off-Noir-W.png?v=1673619115&width=533",
+            cena: "2399zł",
+            opis: "AIR JORDAN 5 GORE TEX"
         },
     ]
 
@@ -51,7 +51,7 @@ function ubrania(){
             article.appendChild(h2);
             article.appendChild(p);
 
-            document.body.appendChild(article);
+            document.getElementById("main").appendChild(article);
         }
 
 
