90d415f293411eb4d0afec7b689eaf13c42fa5ed
skroboltymek <Tymoteusz.skrobol@gmail.com>
2024-03-14 10:13:01 +0100

    menu

--- scripts.js
+++ scripts.js
@@ -53,28 +53,8 @@ function ubrania(){
 
             document.getElementById("mid").appendChild(article);
         }
+
+
 }
+
 ubrania();
-function updateclock(){
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
-
-    if(hours < 10){
-        hours = "0" + hours;
-    }else{
-        hours = hours;
-    }
-    
-    minutes = (minutes<10) ? "0" + minutes : minutes;
-    seconds = (seconds<10) ? "0" + seconds : seconds;
-
-    document.getElementById("hours").textContent = hours + ":";
-    document.getElementById("minutes").textContent = minutes + ":";
-    document.getElementById("seconds").textContent = seconds;
-
-}
-
-setInterval(updateclock,1000)
-updateclock();
